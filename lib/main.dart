d7d7d2791ca4777e43010bc4c3ad4bcaa7b6577e
SuchiMitra <suchi5862m@gmail.com>
2022-01-10 01:37:27 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hero_animation/todomodel.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+      home: ChangeNotifierProvider(
+        create: (context) => TodoModel(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('every time clicks');
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.lightBlue[900],
+        title: Text('state management - Provider '),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+      ),
+      backgroundColor: Colors.lightBlue[900],
+      body: Padding(
+        padding: EdgeInsets.all(1.0),
+        child: Column(
+          children: [
+            Text(
+              '4.45 PM',
+              style:
+                  GoogleFonts.roboto(fontSize: 45, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'current time',
+              style: GoogleFonts.raleway(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    ),
+                  ),
+                  child: Consumer<TodoModel>(builder: (context, todo, child) {
+                    return ListView.builder(
+                        itemCount: todo.taskList.length,
+                        itemBuilder: (context, int index) {
+                          return Container(
+                            padding: EdgeInsets.all(2),
+                            child: ListTile(
+                              title: Text(todo.taskList[index].title),
+                              subtitle: Text(todo.taskList[index].detail),
+                              trailing: Icon(
+                                Icons.check_circle_rounded,
+                                color: Colors.lightGreen,
+                              ),
+                            ),
+                          );
+                        });
+                  })),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<TodoModel>(context, listen: false).addTaskList();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/todomodel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:hero_animation/taskmodel.dart';
+
+import 'package:provider/provider.dart';
+
+class TodoModel extends ChangeNotifier {
+  List<TaskModel> taskList = [];
+
+  addTaskList() {
+    TaskModel taskModel = TaskModel('Title of ${taskList.length}',
+        'details  of the to do list ${taskList.length}');
+    taskList.add(taskModel);
+    notifyListeners();
+  }
+}
